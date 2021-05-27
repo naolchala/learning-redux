6c7e71da9af3af6fd779c56a8dfad8649d73ee59
naolchala <naolchala6@gmail.com>
2021-05-28 01:28:18 +0300

    This Branch is for github hosting

--- src/Components/Data/Store/index.js
+++
@@ -1,6 +0,0 @@
-import {createStore} from 'redux';
-import taskReducer from '../Reducers';
-
-const store = createStore(taskReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-export default store;
